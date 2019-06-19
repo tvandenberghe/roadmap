c4dc87796b88f4e2cee243cd383ad3aa4dfc236c
Thomas Vandenberghe <tvandenberghe@naturalsciences.be>
2019-06-19 13:07:38 +0200

    move to sql seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-# db/seeds.rb
-
 def execute_sql_file(path, connection = ActiveRecord::Base.connection)
   require 'active_support/core_ext/object/blank.rb'
   IO.read(path).split(';').reject(&:blank?).each do |statement|
