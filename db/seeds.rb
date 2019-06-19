d8e8f18704dd56697ccefc2e5d60923866a8e9d2
Thomas Vandenberghe <tvandenberghe@naturalsciences.be>
2019-06-19 13:37:56 +0200

    move to sql seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 #unless Rails.env.production?
-  connection = ActiveRecord::Base.connection
-  connection.tables.each do |table|
-    connection.execute("TRUNCATE #{table} CASCADE" ) unless table == "schema_migrations"
-  end
+#  connection = ActiveRecord::Base.connection
+#  connection.tables.each do |table|
+#    connection.execute("TRUNCATE #{table} CASCADE" ) unless table == "schema_migrations"
+#  end
 
-  sql = File.read('db/seeds.sql')
-  statements = sql#.split(/;$/)
-  #statements.pop
+#  sql = File.read('db/seeds.sql')
+#  statements = sql#.split(/;$/)
+#  #statements.pop
 
-  ActiveRecord::Base.transaction do
-    connection.execute(statements)
+#  ActiveRecord::Base.transaction do
+#    connection.execute(statements)
     #statements.each do |statement|
     #  connection.execute(statement)
     #end
-  end
+#  end
 #end
