29b2f190b82c83cf08bcad770275fa85e71cf188
Thomas Vandenberghe <tvandenberghe@naturalsciences.be>
2019-07-09 10:50:56 +0200

    fix env not stored on remote

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #compile the assets
 rake assets:precompile
 rake webpacker:compile
+echo "SECRET_KEY_BASE: $SECRET_KEY_BASE"
 # Start the server
-rails server -b '0.0.0.0' -p 8080
+rails server -b 0.0.0.0
 
